3292d9b1c9a144146fb679becc3a7dd72656bac8
JohnPham <ThanhP@joblogic.com>
2023-10-30 23:31:38 +0700

    Initial Commit

---
+++ Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by JohnPham on 10/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
