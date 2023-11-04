978f687f57ff9391cc119c82d77e4422852b3d89
JohnPham <ThanhP@joblogic.com>
2023-11-04 11:02:03 +0700

    Format code

--- Note/NoteViewController.swift
+++ Note/NoteViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class NoteViewController: UIViewController {
-
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var contentLabel: UITextView!
+    
+    public var noteTitle:String = ""
+    public var noteContent: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        titleLabel.text = noteTitle
+        contentLabel.text = noteContent
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- Note/ViewController.swift
+++ Note/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    @IBOutlet var label: UILabel!
+    
+    var notes: [(title: String, note: String)] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
+        title = "Notes"
     }
-
-
+    
+    @IBAction func didTapNewNote(){
+        guard let vc = storyboard?.instantiateViewController(identifier: "new") as? EntryViewController else{
+            return
+        }
+        vc.title = "New Note"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.completetion = {noteTitle, note in self.notes.append((title: noteTitle, note: note))
+            self.navigationController?.popViewController(animated: true)
+            self.label.isHidden = true
+            self.table.isHidden = false
+            self.table.reloadData()
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    //Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = notes[indexPath.row].title
+        cell.detailTextLabel?.text = notes[indexPath.row].note
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let note = notes[indexPath.row]
+        
+        // Show Note Controller
+        guard let vc = storyboard?.instantiateViewController(identifier: "note") as? NoteViewController else {
+            return
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.title = "Notes"
+        vc.noteTitle = note.title
+        vc.noteContent = note.note
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
 
